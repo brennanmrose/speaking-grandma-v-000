2e12976c6f63bf372c7c18b9aff0911c9bcf5213
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-17 01:28:49 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -9,4 +9,11 @@ def speak_to_grandma(phrase)
 end 
 
 def speak_to_grandma(phrase)
-  if 
+  if phrase = "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
